ee5038839a3a35c6e756468b76f187ff78be0ade
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-17 18:44:19 +0200

    Add js sistem to read value input and compare  whit random namber to incres score

--- js/main.js
+++ js/main.js
@@ -6,54 +6,89 @@ Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
 //variabile bottone x iniziare a giocare
-const StartBotton= document.getElementById("wanna-play");
-
+const StartBotton = document.getElementById("wanna-play");
 
 // variabile da cui andremo a togliere una classe
-let PlayTable=document.getElementById("playtable");
+let PlayTable = document.getElementById("playtable");
 
 //variabile per inserire i numeri generati
-let NumbersPlay=document.getElementById("table-game");
- let Num = Random (100,1,5);
- NumbersPlay.append(Num)
- console.log(Num);
+let NumbersPlay = document.getElementById("table-game");
+
+//variabile send
+let Send = document.getElementById("send-result");
+//variabile input
+let NumberAnswer = document.querySelector("input");
+// genero una array con i numeri randomici
+let Num = Random(100, 1, 5);
+console.log(Num);
+//Stampo in pagina l'array dei numeri
+NumbersPlay.append(Num);
+
 //evento a click scompare il bottone ed appare il gioco
-StartBotton.addEventListener("click",function () {
-    PlayTable.classList.remove("hide");
-    StartBotton.classList.add("hide");
-    setInterval(function(){NumbersPlay.classList.add("hide")} ,30000);
-})
+StartBotton.addEventListener("click", function () {
+  //aggiungo e tolgo diplay non alle parti che mi interessano
+  PlayTable.classList.remove("hide");
+  StartBotton.classList.add("hide");
+
+  // setto un intervallo di tempo dopo il quale verra aggiunta la classe hide
+  // 30 secondi dovrebbero corrispondere a 30 *1000
+  setInterval(function () {
+    NumbersPlay.classList.add("hide");
+  }, 10000);
+});
+const Answer = [];
+
+let punteggio = 0;
+
+Send.addEventListener("click", function () {
+  let NumberUser = parseInt(document.querySelector("input").value);
+  if (!Answer.includes(NumberUser)) {
+    Answer.push(NumberUser);
+    if (Num.includes(NumberUser)) {
+      punteggio++;
+    }
+    NumberAnswer.value = "";
+  }
+  console.log(Answer);
+  console.log(punteggio);
+  if (Answer.length === 5) {
+    Send.classList.add("hide");
+    NumberAnswer.classList.add("hide");
+    document.getElementById("result").innerHTML =
+  " Hai totalizato un punteggio di" + " " + punteggio ;
+
+  }
+});
 
 
 
 
 
+
+
+
+
+
+
+
+function UserNumber(Numberelement) {
+  console.log(UserNumbers);
+  const UserNumbers = [];
+}
 
 // Funzione genera numeri casuali
-function Random(max,min,Numberelement) {
-let RandomNum=[];
-let i=0;
-while (i < Numberelement) {
-     let Numbers= Math.floor((Math.random()* (max-min))+min);
+function Random(max, min, Numberelement) {
+  let RandomNum = [];
+  let i = 0;
+  while (i < Numberelement) {
+    let Numbers = Math.floor(Math.random() * (max - min) + min);
     if (!RandomNum.includes(Numbers)) {
-         RandomNum.push(Numbers); 
-         i++;
+      RandomNum.push(Numbers);
+      i++;
     }
-    
-} 
-    return RandomNum;
+  }
+  return RandomNum;
 }
-
 
 // funzione stampa in
-function Stampa() {
-    
-}
-
-
-
-
-
-
-
-
+function Stampa() {}
